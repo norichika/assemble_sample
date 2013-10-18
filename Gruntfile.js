8812faf66eb35548d1f16336b604a1d35e771148
yamanaka <yamanaka@meme.ne.jp>
2013-10-18 15:08:50 +0900

    テンプレ機能のお試し

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,22 @@
 module.exports = function(grunt) {
   grunt.task.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks("grunt-este-watch");
   grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'dist',
+          keepalive: true
+        }
+      }
+    },
     assemble: {
       site: { // target name
         options: {
-          data: ['src/**/*.yml'] // data files
+          data: ['src/**/*.yml'], // data files
+          partials: 'src/partials/**/*.hbs'
         },
         files: [
           {
@@ -17,5 +29,5 @@ module.exports = function(grunt) {
       }
     }
   });
-  grunt.registerTask('default', ['assemble']);
+  grunt.registerTask('default', ['assemble', 'connect']);
 };
