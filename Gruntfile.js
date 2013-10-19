b56c357f3adc2b208d82a7db666ff62ba7fa900b
norichika yamanaka <ynorichika@gmail.com>
2013-10-19 21:51:51 +0900

    watchを追加

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,23 @@ module.exports = function(grunt) {
   grunt.task.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks("grunt-este-watch");
+
+  // ファイル書き出し設定
+  // 除外ファイル
+  var excludeDir = 'src/pages/'
+  var assembleconf_files =[{
+            expand: true,
+            cwd: excludeDir,
+            src: '**/*.hbs', // source file
+            dest: 'dist/' // compile to
+          }];
   grunt.initConfig({
     connect: {
       server: {
         options: {
           port: 9000,
-          base: 'dist',
-          keepalive: true
+          base: 'dist'
+          // keepalive: true
         }
       }
     },
@@ -18,16 +28,32 @@ module.exports = function(grunt) {
           data: ['src/**/*.yml'], // data files
           partials: 'src/partials/**/*.hbs'
         },
-        files: [
-          {
+        files: assembleconf_files
+      }
+    },
+    esteWatch: {
+      options: {
+        dirs: ['src/pages/**/*.hbs','src/**/*.css','src/**/*.js'],
+        livereload: {
+          enabled: false
+        }
+      },
+      hbs: function(filepath) {
+        var pathget = filepath.replace(excludeDir, '');
+        var conf_files = [{
             expand: true,
-            cwd: 'src/pages/',
-            src: '**/*.hbs', // source file
+            cwd: excludeDir,
+            src: pathget, // source file
             dest: 'dist/' // compile to
-          },
-        ]
+        }];
+        grunt.config.set('assemble.site.files', conf_files);
+        return 'assemble'
+      },
+      css: function(filepath) {
+        return 'connect'
       }
     }
   });
-  grunt.registerTask('default', ['assemble', 'connect']);
+  // grunt.registerTask('default', ['assemble', 'connect']);
+  grunt.registerTask('w', ['esteWatch']);
 };
