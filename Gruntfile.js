013e9129727e31b079c7f945febf371f5b46f943
yamanaka <yamanaka@meme.ne.jp>
2013-11-06 19:23:42 +0900

    CopyとCompass追加

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
   grunt.task.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks("grunt-este-watch");
   grunt.loadNpmTasks("grunt-ftp-push");
 
@@ -12,15 +14,15 @@ module.exports = function(grunt) {
             cwd: excludeDir,
             src: '**/*.hbs', // source file
             dest: 'dist/' // compile to
-          }];
+  }];
 
   // FTP設定
   var ftpSettings = {
     authKey: 'serverA',
-    host: 'sample.server.com',
+    host: '******',
     dest: '/html/assemble/',
     port: 21,
-    files : [ 'dest/**']
+    files : ['dist/**']
   };
 
   grunt.initConfig({
@@ -30,6 +32,25 @@ module.exports = function(grunt) {
           port: 9000,
           base: 'dist'
           // keepalive: true
+        }
+      }
+    },
+    copy: {
+      dist: {
+        files: [
+          {expand: true, cwd: excludeDir, src: ['**/*.css'], dest: 'dist/'}
+        ]
+      }
+    },
+    compass: {
+      common_dev: {
+        options: {
+          // config: 'compass_config.rb'
+          sassDir: 'src/pages/common/sass/',
+          // cssDir: 'dist/common/css/'
+          cssDir: 'src/pages/common/css/'
+          // importPath: 'src/pages/common/_sass/_setting',
+          // environment: "development"
         }
       }
     },
@@ -53,9 +74,11 @@ module.exports = function(grunt) {
     },
     esteWatch: {
       options: {
-        dirs: ['src/pages/**'],
+        dirs: ['src/pages/**', 'dist/**'],
         livereload: {
-          enabled: false
+          enabled: false,
+          port: 35729,
+          extensions: ['html', 'js', 'css']
         }
       },
       hbs: function(filepath) {
@@ -66,20 +89,45 @@ module.exports = function(grunt) {
             src: pathget, // source file
             dest: 'dist/' // compile to
         }];
-        var pathhtml = pathget.replace('.hbs', '.html');
-        var ftppath = 'dist/' + pathhtml;
-        var ftpfile = [ftppath];
-        console.log(ftpfile);
+        // var pathhtml = pathget.replace('.hbs', '.html');
+        // var ftppath = 'dist/' + pathhtml;
+        // var ftpfile = [ftppath];
+        // console.log(ftpfile);
         grunt.config.set('assemble.site.files', conf_files);
-        grunt.config.set('ftp_push.files', ftpfile);
-        return ['assemble', 'ftp_push'];
+        // grunt.config.set('ftp_push.files', ftpfile);
+        return ['assemble'];
+      },
+      scss: function(filepath) {
+        // var pathget = filepath.replace(excludeDir, '');
+        // var pathcss = pathget.replace('.scss', '.css');
+        // pathcss = pathcss.replace('sass', 'css');
+        // var ftppath = 'dist/' + pathcss;
+        // var ftpfile = [ftppath];
+        // console.log(ftpfile);
+        // grunt.config.set('ftp_push.files', ftpfile);
+        return ['compass:common_dev', 'copy'];
+      },
+      html: function(filepath) {
+        var ftphtmlfile = [filepath];
+        grunt.config.set('ftp_push.files', ftphtmlfile);
+        console.log(ftphtmlfile);
+        return ['ftp_push'];
       },
       css: function(filepath) {
-        return 'connect';
+        var distAry = filepath.split('/');
+        var distDir = distAry[0];
+        if(distDir === 'dist') {
+          // var pathget = filepath.replace(excludeDir, '');
+          var ftpcssfile = [filepath];
+          grunt.config.set('ftp_push.files', ftpcssfile);
+          console.log(ftpcssfile);
+          return ['ftp_push'];
+        }
       }
     }
   });
   // grunt.registerTask('default', ['assemble', 'connect']);
   grunt.registerTask('w', ['esteWatch']);
+  grunt.registerTask('cmp', ['compass:common_dev']);
   grunt.registerTask('ftp', ['ftp_push']);
 };
